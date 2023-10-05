8ebfd829935b0400d84b220e42d2096a5fe2883f
Pelle <pdblaauw@student.che.nl>
2023-10-05 15:37:08 +0200

    Code om CSS toe te kunnen voegen

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+app.static_folder = 'static'
+app.static_url_path = "/static"
+
 @app.route('/')
 def index():
     return render_template('index.html')
