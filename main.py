2f96dc998d459d902adfed720d20636b152e6fa2
Pelle <pdblaauw@student.che.nl>
2023-10-10 21:36:14 +0200

    Eerste opzet frontend, moet nog dry gemaakt worden

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
@@ -8,6 +9,14 @@ app.static_url_path = "/static"
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/result')
+def result():
+    return render_template('result.html')
+
+@app.route('/favicon.ico')
+def fav():
+    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico')
 
 if __name__ == '__main__':
     app.run()
